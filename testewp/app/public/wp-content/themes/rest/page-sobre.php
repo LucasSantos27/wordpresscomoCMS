4ecd4a422f74ab3b21a143b7b896e618bddcbd44
unknown <lucas-unifei@outlook.com>
2021-05-02 10:29:08 -0300

    Native Custom Field

--- testewp/app/public/wp-content/themes/rest/page-sobre.php
+++ testewp/app/public/wp-content/themes/rest/page-sobre.php
@@ -2,8 +2,9 @@
 //Template Name: Sobre
 ?>
 <?php get_header(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<section class="container sobre">
-			<h2 class="subtitulo">Sobre</h2>
+			<h2 class="subtitulo"><?php the_title(); ?></h2>
 
 			<div class="grid-8">
 				<img src="img/rest-fachada.jpg" alt="Fachada do Rest">
@@ -11,12 +12,14 @@
 
 			<div class="grid-8">
 				<h2>História</h2>
-				<p>O incentivo ao avanço tecnológico, assim como a mobilidade dos capitais internacionais auxilia a preparação e a composição dos níveis de motivação departamental.</p>
-				<p>Gostaria de enfatizar que o desenvolvimento contínuo de distintas formas de atuação prepara-nos para enfrentar situações atípicas decorrentes do remanejamento dos quadros funcionais.</p>
+				<p><?php echo get_post_meta(get_the_ID(), 'historia', true); ?></p>
+				<p><?php echo get_post_meta(get_the_ID(), 'historia2', true); ?></p>
 				<h2>Visão</h2>
-				<p>Não obstante, a expansão dos mercados mundiais faz parte de um processo de gerenciamento de alternativas às soluções ortodoxas.</p>
+				<p><?php echo get_post_meta(get_the_ID(), 'visao', true); ?></p>
 				<h2>Valores</h2>
 				<p>O empenho em analisar a consolidação das estruturas apresenta tendências no sentido de aprovar a manutenção dos índices pretendidos.</p>
 			</div>
 		</section>
-	<?php get_footer(); ?>
+<?php endwhile;
+else : endif; ?>
+<?php get_footer(); ?>
