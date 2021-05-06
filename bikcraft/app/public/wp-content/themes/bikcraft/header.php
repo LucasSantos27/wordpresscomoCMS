066e479a21eeb34b6a5fc94300c901678c500f6a
unknown <lucas-unifei@outlook.com>
2021-05-06 16:02:10 -0300

    Scripts and CSS

--- bikcraft/app/public/wp-content/themes/bikcraft/header.php
+++ bikcraft/app/public/wp-content/themes/bikcraft/header.php
@@ -16,9 +16,6 @@
 
 	<link rel="shortcut icon" href="favicon.ico">
 
-	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
-	<script src="js/libs/modernizr.custom.45655.js"></script>
-
 	<!-- Inicio Wordpress Header -->
 	<?php wp_head(); ?>
 	<!-- Final Wordpress Header -->
